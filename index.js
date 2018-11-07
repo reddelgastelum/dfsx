bde5207f0e08b05b801f8dcbb15585f092c09d36
Reddel Gastelum <reddelgastelum@gmail.com>
2018-11-07 05:58:07 -0800

    Completed the scorestrip module

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 let Scorestrip = require('./modules/Scorestrip.js');
 
-let scorestrip = new Scorestrip('18', 'REG', '8');
-console.log(scorestrip.getBody());
+function getEids(team, weeks, season) {
+  let eids = [];
+  let ss = null;
+  let currentWeek = 99;
+
+  while (eids.length < weeks && currentWeek > 0) {
+
+    if (currentWeek == 99) {
+      ss = new Scorestrip();
+    } else {
+      ss = new Scorestrip({season: season, seasonType: 'REG', week: currentWeek})
+    }
+
+    currentWeek = Number(ss.getData().w);
+    let eid = ss.getEidFrom(team);
+
+    if (eid) {
+      eids.push(eid);
+      currentWeek = currentWeek - 1;
+    } else {
+      currentWeek = currentWeek - 1;
+    }
+  }
+
+  return eids;
+}
+
+console.log(getEids('GB', 16, '2018'));
--- modules/Scorestrip.js
+++ modules/Scorestrip.js
@@ -1,14 +1,40 @@
+/*
+  The Scorestrip class takes an options object. For example
+
+  {
+    season: '2018',
+    seasonType: 'REG',
+    week: '8'
+  }
+
+  it returns all the data that scorestrip returns but in json instead of xml
+
+*/
+
 let request = require('sync-request');
+let convert = require('xml-js');
 
 class Scorestrip {
-  constructor(season, seasonType, week) {
-    this.season = season;
-    this.seasonType = seasonType;
-    this.week = week;
+  constructor(options = {}) {
+    this.season = options.season;
+    this.seasonType = options.seasonType;
+    this.week = options.week;
+  }
+
+  current() {
+    if (this.season == undefined || this.seasonType == undefined || this.week == undefined) {
+      return true;
+    }
+    return false;
   }
 
   getBody() {
-    let url = 'http://www.nfl.com/ajax/scorestrip?season='+ this.season +'&seasonType='+ this.seasonType +'&week='+ this.week;
+    let url;
+    if (this.current()) {
+      url = 'http://www.nfl.com/liveupdate/scorestrip/ss.xml';
+    } else {
+      url = 'http://www.nfl.com/ajax/scorestrip?season='+ this.season +'&seasonType='+ this.seasonType +'&week='+ this.week;
+    }
     let response = request('GET', url);
     if (response.statusCode == 200) {
       return response.getBody();
@@ -17,8 +43,67 @@ class Scorestrip {
     }
   }
 
-  getJson() {
+  getData() {
     let body = this.getBody();
+    let data = convert.xml2json(body, {compact: true, spaces: 2});
+    let json = JSON.parse(data)['ss']['gms'];
+
+    let gd = json['_attributes']['gd'];
+    let w = json['_attributes']['w'];
+    let y = json['_attributes']['y'];
+    let t = json['_attributes']['t'];
+    let gmsarr = json['g'];
+    let gms = [];
+
+    for (let i = 0; i < gmsarr.length; i++) {
+      let game = gmsarr[i]['_attributes'];
+
+      gms.push(game);
+    }
+
+    return {
+      gd,
+      w,
+      y,
+      t,
+      gms
+    };
+
+  }
+
+  isDone() {
+    let data = this.getData();
+    let games = data.gms;
+    let results = [];
+
+    for (let i = 0; i < games.length; i++) {
+      if (games[i].q == 'F' || games[i].q == 'FO') {
+        results.push(true);
+      } else {
+        break;
+      }
+    }
+
+    if (games.length == results.length) {
+      return true;
+    }
+    return false;
+  }
+
+  getEidFrom(team) {
+    let data = this.getData();
+    let games = data.gms;
+    let result = null;
+    for (let i = 0; i < games.length; i++) {
+
+      if ((games[i].h == team || games[i].v == team) && (games[i].q == 'F' || games[i].q == 'FO')) {
+        result = games[i].eid;
+        break;
+      } else {
+        result = false;
+      }
+    }
+    return result;
   }
 }
 
