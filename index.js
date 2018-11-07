21fe30b99a9e9f781b362a8e5329848c035db847
Reddel Gastelum <reddelgastelum@gmail.com>
2018-11-07 06:06:20 -0800

    Created helpers file and added the getEids function

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-let Scorestrip = require('./modules/Scorestrip.js');
+let getEids = require('./modules/helpers.js').getEids;
 
-function getEids(team, weeks, season) {
-  let eids = [];
-  let ss = null;
-  let currentWeek = 99;
-
-  while (eids.length < weeks && currentWeek > 0) {
-
-    if (currentWeek == 99) {
-      ss = new Scorestrip();
-    } else {
-      ss = new Scorestrip({season: season, seasonType: 'REG', week: currentWeek})
-    }
-
-    currentWeek = Number(ss.getData().w);
-    let eid = ss.getEidFrom(team);
-
-    if (eid) {
-      eids.push(eid);
-      currentWeek = currentWeek - 1;
-    } else {
-      currentWeek = currentWeek - 1;
-    }
-  }
-
-  return eids;
-}
-
-console.log(getEids('GB', 16, '2018'));
+console.log(getEids('GB', 2, '2018'));
---
+++ modules/helpers.js
@@ -0,0 +1,32 @@
+let Scorestrip = require('./Scorestrip.js');
+
+let helpers = {};
+
+helpers.getEids = function getEids(team, weeks, season) {
+  let eids = [];
+  let ss = null;
+  let currentWeek = 99;
+
+  while (eids.length < weeks && currentWeek > 0) {
+
+    if (currentWeek == 99) {
+      ss = new Scorestrip();
+    } else {
+      ss = new Scorestrip({season: season, seasonType: 'REG', week: currentWeek})
+    }
+
+    currentWeek = Number(ss.getData().w);
+    let eid = ss.getEidFrom(team);
+
+    if (eid) {
+      eids.push(eid);
+      currentWeek = currentWeek - 1;
+    } else {
+      currentWeek = currentWeek - 1;
+    }
+  }
+
+  return eids;
+}
+
+module.exports = helpers;
